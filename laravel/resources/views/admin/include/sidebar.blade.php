de8a4ea8ee932c90232098a24836eee11c487177
ilona <ilonatepavicharova@gmail.com>
2025-11-12 19:08:30 +0300

    Урок 6

---
+++ laravel/resources/views/admin/include/sidebar.blade.php
@@ -0,0 +1,20 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-2 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="{{route('admin.category.index')}}" class="nav-link">
+                <i class="nav-icon fas fa-list-alt"></i>
+                <p>
+                    Категории
+                </p>
+            </a>
+        </li>
+        </ul>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -21,4 +21,9 @@ Route::group(["namespace" => 'App\Http\Controllers\Admin', 'prefix'=> 'admin'], 
     Route::group(["namespace" => 'Main'], function () {
         Route::get("/", "IndexController")->name("index");
     });
+    Route::group(["namespace" => 'Category', 'prefix' => 'categories'], function () {
+        Route::get("/", "IndexController")->name("admin.category.index");
+        Route::get("/create", "CreateController")->name("admin.category.create");
+        Route::post("/", "StoreController")->name("admin.category.store");
+    });
 });
